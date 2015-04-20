a6e62d728f19c66ccb4faef81b54215fd0ef1bd2
Niems <czniemann1990@gmail.com>
2015-04-20 04:14:16 -0500

    Added multiple submenus

--- Swingin.java
+++ Swingin.java
@@ -24,6 +24,13 @@ public class Swingin extends JFrame implements ActionListener{
      * @param args the command line arguments
      */
     private static final String file = "File";
+    
+    private static final String level = "Level";
+    private static final String level_blank = "_";
+    private static final String level_one = "Level One";
+    private static final String level_two = "Level Two";
+    private static final String level_three = "Level Three";
+    
     private static final String exit = "Exit";
     
     private static JMenuBar menubar;
@@ -33,26 +40,57 @@ public class Swingin extends JFrame implements ActionListener{
     public Swingin(){
         super("Swing Test");
         setLayout(new FlowLayout());
-        
+        JMenuItem item;
+        JMenu menu, menu2, menu3;
         Swingin.menubar = new JMenuBar();
         
         Swingin.menu_file = new JMenu(file);
+        menu = new JMenu(level);
+        item = new JMenuItem(level_blank);  
+        
+        menu2 = new JMenu(level_one);
+        menu2.add(item);
+        item = new JMenuItem(level_blank);  
+        menu2.add(item);
+        
+        item = new JMenuItem(level_blank);  
+        menu.add(item);
+        menu.add(menu2);
+        
+        menu3 = new JMenu(level_two);
+        item = new JMenuItem(level_blank);
+        menu3.add(item);
+        
+        item = new JMenuItem(level_blank);
+        menu3.add(item);
+        
+        item = new JMenuItem(level_blank);
+        menu3.add(item);
+        
+        item = new JMenuItem("The End");
+        menu3.add(item);
+        menu2.add(menu3);
+        Swingin.menu_file.add(menu);
         
         
         
+        
+        /*
         JMenu level_1 = new JMenu("Level 1");
-        JMenuItem level_item = new JMenuItem("Blank");
+        JMenuItem level_item = new JMenuItem(level_blank);
         level_1.add(level_item);
         
         level_item = new JMenuItem("Level 2");
         level_1.add(level_item);
         
         Swingin.menu_file.add(level_1);
+        */
         
         Swingin.menu_item = new JMenuItem("Exit");
         Swingin.menu_item.setMnemonic('x');
         Swingin.menu_item.addActionListener(Swingin.this);
         Swingin.menu_file.add(Swingin.menu_item);
+        
         
         menubar.add(Swingin.menu_file);
         this.setJMenuBar(menubar);
